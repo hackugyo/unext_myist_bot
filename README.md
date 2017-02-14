aabcd191877eb3bb00d813823b365f7a0d4abb5c
hackugyo <game6000yen@gmail.com>
2017-02-14 18:58:05 +0900

    Say something.

---
+++ README.md
@@ -0,0 +1,7 @@
+## How to use
+
+```
+$ source application.env # Make your own one
+$ bundle install --path vendor/bundler
+$ ruby unext.rb > result.csv
+```
--- bot.rb
+++ bot.rb
@@ -4,5 +4,10 @@ Bundler.require
 require 'http'
 require 'json'
 
-response = HTTP.post("https://slack.com/api/api.test")
+response = HTTP.post("https://slack.com/api/chat.postMessage", params: {
+                       token: ENV['HUBOT_SLACK_TOKEN'],
+                       channel: "#general",
+                       text: "UNEXT ！",
+                       as_user: true,
+                     })
 puts JSON.pretty_generate(JSON.parse(response.body))
